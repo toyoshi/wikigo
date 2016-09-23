8a387a81d18af8094a3e1b5a752be184b21c5175
toyoshi <toyosystem@gmail.com>
2016-09-24 00:28:24 +0900

    キーワードでアクセス、リンクできるようにした

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,2 +1,13 @@
 module WordsHelper
+  def add_word_link(body)
+    word_list.each do |w|
+      body.sub!(w, link_to(w, url_for(controller: :words, action: :show, id: w)))
+    end
+    body
+  end
+
+  private 
+  def word_list
+    Word.all.pluck(:title)
+  end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,13 @@
 class Word < ApplicationRecord
+  def self.find(input)
+    if input.is_a?(Integer)
+      super
+    else
+      find_by_title(input)
+    end
+  end
+
+  def to_param
+    title
+  end
 end
