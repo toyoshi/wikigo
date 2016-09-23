5a69ae3a0343c8ba8324e8966ca266eb8b66f357
toyoshi <toyosystem@gmail.com>
2016-09-24 00:56:23 +0900

    markdownで表示されるようにした

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,4 +1,11 @@
 module WordsHelper
+  
+  @@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+
+  def markdown(str)
+    @@markdown.render(str)
+  end
+
   def add_word_link(body)
     word_list.each do |w|
       body.sub!(w, link_to(w, url_for(controller: :words, action: :show, id: w)))
