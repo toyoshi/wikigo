ed90e684b1d5a854775a67e0f75109c5e1a31137
toyoshi <toyosystem@gmail.com>
2016-09-24 00:39:47 +0900

    rootのrouting定義と、/wikiでwordsコントローラーにアクセスできるように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :words
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'words#index'
+  resources :words, path: :wiki
 end
