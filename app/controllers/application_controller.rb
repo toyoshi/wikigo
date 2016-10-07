dc10597b26f21483329cd89fc5a1d015300f97de
toyoshi <toyosystem@gmail.com>
2016-10-07 22:37:39 +0900

    _menuをサイドバーに出すようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_global_word
+
+  def set_global_word
+    @menu = Word.find_or_create_by(title: '_menu')
+  end
 end
