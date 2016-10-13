151664081c0fea09957fa45520779388fa6cc76a
toyoshi <toyosystem@gmail.com>
2016-10-14 01:39:06 +0900

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.find_or_create_by(id: 1) do |user|
   user.email = 'wiki@example.com'
-  user.password = 'letseditwiki'
+  user.password = 'password'
 end
 
-Word.find_or_create_by(title: '_main') do |word|
-  word.title = '_main'
-  word.body = ""
+Word.find_or_create_by(title: 'Main Page') do |word|
+  word.title = 'Main Page'
+  word.body = "Wiki wiki go!"
 end
