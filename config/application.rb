e42eb8dbd8b2835c4e56b8b520c216116e98f147
toyoshi <toyosystem@gmail.com>
2016-10-13 22:48:17 +0900

    ユーザー登録をできないようにした

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Wikigo
   class Application < Rails::Application
-    config.to_prepare do
-        Devise::SessionsController.layout 'users'
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
