731dfcce4ea1e87414d63886d61932f98a52de48
toyoshi <toyosystem@gmail.com>
2016-10-17 23:54:28 +0900

    Edit layout file

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_word, only: [:show, :version, :edit, :update, :destroy]
 
   # GET /words
