21fe7649e2d7130c370cee8d77d1c3b73c4526ca
toyoshi <toyosystem@gmail.com>
2016-10-26 15:58:21 +0900

    Add role column to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: { editor: 0, admin: 99 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User default role" do
+    @user = users( :john ) 
+    assert_equal false, @user.admin?
+  end
 end
