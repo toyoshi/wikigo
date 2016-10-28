9e66f854e57c6f856ef1be68f883a0877ff7b9cd
toyoshi <toyosystem@gmail.com>
2016-10-29 07:09:33 +0900

    seedsの初期化の方法のバグを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@ Word.find_or_create_by(title: 'Main Page') do |word|
 end
 
 #Site Title
-Option.site_title = 'Wiki Go' unless Option.site_title.blank?
+Option.site_title = 'Wiki Go' if Option.site_title.blank?
 
 #Theme
 Option.theme = '' if Option.theme.blank?
 
 #Default User Registration Token
-Option.update_registration_token unless Option.user_registration_token.blank?
-Option.list_size_of_recent_words_parts = 5 unless Option.list_size_of_recent_words_parts.blank?
+Option.update_registration_token if Option.user_registration_token.blank?
+Option.list_size_of_recent_words_parts = 5 if Option.list_size_of_recent_words_parts.blank?
