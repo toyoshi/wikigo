eb6fbebb37d21e4e562aea5827a3eee372c0e545
toyoshi <toyosystem@gmail.com>
2016-11-01 01:46:57 +0900

    Add authenticate to versions controller

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,4 +1,5 @@
 class VersionsController < ApplicationController
+  before_action :authenticate_user!, only: [:rollback]
   before_action :set_word, only: [:show, :rollback, :index, :destroy]
 
   def rollback
