885c62f6dee2aace1d81d7c5871194d135d6de78
toyoshi <toyosystem@gmail.com>
2016-11-01 09:34:33 +0900

    Fix bug of show diff

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -3,11 +3,15 @@ class VersionsController < ApplicationController
   before_action :set_word, only: [:show, :rollback, :index, :destroy]
 
   def rollback
-    @word.save
-    redirect_to word_path(@word)
+    word = @version
+    word.save
+    redirect_to word_path(word)
   end
 
   def show
+    # params[:id] == 0 means to show diff with current_version 
+    # (Paper Trail dose not keep current version)
+    @version = (params[:id] == '0') ? @word : @word.versions.find(params[:id]).reify
   end
 
   def index
@@ -17,8 +21,5 @@ class VersionsController < ApplicationController
   private
   def set_word
     @word = Word.find(params[:word_id])
-    if params[:id]
-      @word = @word.versions.find(params[:id]).reify
-    end
   end
 end
