b9fdc9d882c22aa143d37e710374342835707d74
toyoshi <toyosystem@gmail.com>
2016-11-08 22:02:40 +0900

    Remove the table of content

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,25 +1,13 @@
-class MyRedcarpet < Redcarpet::Render::HTML
-  def header(text, header_level)
-    %Q{<h#{header_level} id="#{text.downcase.gsub(" ", "-")}">#{text}</h#{header_level}>}.html_safe
-  end
-end
-
 module WordsHelper
   extensions = {strikethrough: true, fenced_code_blocks: true}
   @@markdown = Redcarpet::Markdown.new(
-      MyRedcarpet.new(
+      Redcarpet::Render::HTML.new(
         hard_wrap: true,
-        with_toc_data: true,
       ),
     extensions)
-  @@toc = Redcarpet::Markdown.new Redcarpet::Render::HTML_TOC
 
   def markdown(str)
     @@markdown.render(str)
-  end
-
-  def toc(str)
-    @@toc.render(str)
   end
 
   def add_word_link(body)
