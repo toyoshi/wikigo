379a15cb0a9a670cf6b1893d92941af4aa77eb46
toyoshi <toyosystem@gmail.com>
2016-11-17 23:15:48 +0900

    Refactor zip export

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -28,20 +28,30 @@ class SiteController < ApplicationController
 
   def export
     export_path = Rails.root.join('tmp') # for Heroku https://devcenter.heroku.com/articles/cedar-migration
-    zipfile_name = Tempfile.new('export-', export_path)
+
+
+    zipfile_name = Tempfile.new('export-', temp_dir)
 
     Zip::File.open(zipfile_name.path, Zip::File::CREATE) do |zipfile|
       Word.all.find_in_batches do |batch|
         batch.each do |w|
-          temp_file = Tempfile.new('md-', export_path)
+          temp_file = Tempfile.new('md-', temp_dir)
           temp_file.puts w.to_middleman
-          temp_file.close
           zipfile.add("#{w.title}_#{w.id}.md", temp_file.path)
+          temp_file.close
         end
       end
     end
 
     send_file(zipfile_name, filename: 'export.zip')
     return
+  end
+
+  private 
+
+  def temp_dir
+    path = Rails.root.join('tmp')
+    Dir.mkdir(path) unless Dir.exists?(path)
+    path
   end
 end
